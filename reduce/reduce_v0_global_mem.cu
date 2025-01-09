29610b9425af7ba51db911d118b66fef77a85b5c
heru.wang@duke.edu <heru.wang@duke.edu>
2025-01-10 00:33:59 +0800

    backbone

---
+++ reduce/reduce_v0_global_mem.cu
@@ -0,0 +1,75 @@
+#include <cstdio>
+#include <cuda.h>
+#include <stdlib.h>
+#include <cuda_runtime.h>
+
+#define THREAD_PER_BLOCK 256
+
+__global__ void reduce0(float *d_input, float *d_output)
+{
+}
+
+bool check(float *output, float *res, int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        if (abs(output[i] - res[i]) > 0.0005)
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    const int N = 32 * 1024 * 1024;
+    float *input = (float *)malloc(N * sizeof(float));
+    float *d_input;
+    // Use cudaMalloc to alloc memory on gpu. Using double ptr for DIRECTLY modify the original d_input(make this address to point to the alloc'd gpu mem) more or less like using inference.
+    cudaMalloc((void **)&d_input, N * sizeof(N));
+
+    int block_num = N / THREAD_PER_BLOCK;
+    float *output = (float *)malloc(block_num * sizeof(float));
+    float *d_output;
+    cudaMalloc((void **)&d_output, block_num * sizeof(float));
+
+    float *res = (float *)malloc(block_num * sizeof(float));
+
+    for (int i = 0; i < N; ++i)
+    {
+        input[i] = drand48();
+    }
+
+    // Using cpu compute naive reduce result
+    for (int i = 0; i < block_num; ++i)
+    {
+        for (int j = 0; j < THREAD_PER_BLOCK; ++j)
+        {
+            res[i] += input[i * THREAD_PER_BLOCK + j];
+        }
+    }
+
+    cudaMemcpy(d_input, input, N * sizeof(float), cudaMemcpyHostToDevice);
+
+    dim3 Grid(block_num, 1);
+    dim3 Block(THREAD_PER_BLOCK, 1);
+    reduce0<<<Grid, Block>>>(d_input, d_output);
+
+    cudaMemcpy(output, d_output, block_num * sizeof(float), cudaMemcpyDeviceToHost);
+
+    if (check(output, res, block_num))
+        printf("Correct!\n");
+    else
+    {
+        printf("Incorrect!\n");
+        for (int i = 0; i < block_num; ++i)
+        {
+            if (output[i] != res[i])
+            {
+                printf("Incorrect element: %lf (exp: %lf) in index %i\n", output[i], res[i], i);
+            }
+        }
+    }
+
+    cudaFree(d_input);
+    cudaFree(d_output);
+}
