af93066492232735f8e416d2d2e0f7992e3473ee
heru.wang@duke.edu <heru.wang@duke.edu>
2025-01-18 00:30:52 +0800

    sgemm_v0_global_mem

---
+++ build/sgemm/CMakeFiles/sgemm.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sgemm.dir/src/sgemm.cu.o"
+  "CMakeFiles/sgemm.dir/src/sgemm.cu.o.d"
+  "CMakeFiles/sgemm.dir/src/sgemm_v0_global_mem.cu.o"
+  "CMakeFiles/sgemm.dir/src/sgemm_v0_global_mem.cu.o.d"
+  "CMakeFiles/sgemm.dir/src/utils.cpp.o"
+  "CMakeFiles/sgemm.dir/src/utils.cpp.o.d"
+  "sgemm"
+  "sgemm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/sgemm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/sgemm/CMakeFiles/sgemm_v0_global_mem.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sgemm_v0_global_mem.dir/src/sgemm_v0_global_mem.cu.o"
+  "CMakeFiles/sgemm_v0_global_mem.dir/src/sgemm_v0_global_mem.cu.o.d"
+  "CMakeFiles/sgemm_v0_global_mem.dir/src/utils.cpp.o"
+  "CMakeFiles/sgemm_v0_global_mem.dir/src/utils.cpp.o.d"
+  "sgemm_v0_global_mem"
+  "sgemm_v0_global_mem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/sgemm_v0_global_mem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
